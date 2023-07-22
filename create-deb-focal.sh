19e03c16584bdc1232510169b02d64a15979acf4
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2023-07-21 20:22:50 -0400

    push focal script changes

--- create-deb-focal.sh
+++ create-deb-focal.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DIRECTORY="/github/workspace"
+DIRECTORY=$(pwd)
 export DEBIAN_FRONTEND=noninteractive
 
 LATESTCOMMIT=`cat $DIRECTORY/commit-focal.txt`
 
 function error() {
 	echo -e "\e[91m$1\e[39m"
-    rm -f $COMMITFILE
-    rm -rf $DIRECTORY/box64
+  rm -f $COMMITFILE
+  rm -rf $DIRECTORY/box64
 	exit 1
  	break
 }
@@ -16,15 +16,6 @@ function error() {
 rm -rf $DIRECTORY/box64
 
 cd $DIRECTORY
-
-# install dependencies
-apt-get update
-apt-get install wget git build-essential python3 make gettext pinentry-tty sudo devscripts dpkg-dev -y || error "Failed to install dependencies"
-git clone https://github.com/ryanfortner/checkinstall || error "Failed to clone checkinstall repo"
-cd checkinstall
-sudo make install || error "Failed to run make install for Checkinstall!"
-cd .. && rm -rf checkinstall
-sudo apt install -yf ./resources/cmake_3.24.2-25.1_arm64.deb || error "Failed to install latest cmake package!"
 
 rm -rf box64
 
@@ -44,22 +35,28 @@ echo "Wrote commit to commit-focal.txt file for use during the next compilation.
 
 targets=(RK3588)
 
+# Update this var with each added target in the repo.
+# Ensures that no two builds can be installed together.
+alltargets=(ARM64 ANDROID RPI4ARM64 RPI3ARM64 TEGRAX1 RK3399 RK3588)
+
 for target in ${targets[@]}; do
   echo "Building $target"
 
   cd "$DIRECTORY/box64"
   sudo rm -rf build && mkdir build && cd build || error "Could not move to build directory"
+  # allow installation even on x86_64 (needed for checkinstall)
+  sed -i "s/NOT _x86 AND NOT _x86_64/true/g" ../CMakeLists.txt
   # warning, BOX64 cmakelists enables crypto with the ARM_DYNAREC options, it was purly by luck that no crypto opts were used which would be a problem since the Pi4 doesn't have them
   if [[ $target == "ANDROID" ]]; then
-    cmake .. -DBAD_SIGNAL=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DARM_DYNAREC=1 || error "Failed to run cmake."
+    cmake .. -DBAD_SIGNAL=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-9 -DARM_DYNAREC=ON || error "Failed to run cmake."
   else
-    cmake .. -D$target=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DARM_DYNAREC=1 || error "Failed to run cmake."
+    cmake .. -D$target=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-9 -DARM_DYNAREC=ON || error "Failed to run cmake."
   fi
-  make -j4 || error "Failed to run make."
+  make -j8 || error "Failed to run make."
 
   function get-box64-version() {
     if [[ $1 == "ver" ]]; then
-      export BOX64VER="$(./box64 -v | cut -c21-25)"
+      export BOX64VER="$(qemu-aarch64-static ./box64 -v | cut -c21-25)"
     elif [[ $1 == "commit" ]]; then
       export BOX64COMMIT="$commit"
     fi
@@ -80,10 +77,10 @@ for target in ${targets[@]}; do
   systemctl restart systemd-binfmt || true" > postinstall-pak || error "Failed to create postinstall-pak!"
 
   conflict_list="qemu-user-static"
-  for value in "${targets[@]}"; do
+  for value in "${alltargets[@]}"; do
     [[ $value != $target ]] && conflict_list+=", box64-$(echo $value | tr '[:upper:]' '[:lower:]' | tr _ - | sed -r 's/ /, /g')"
   done
-  sudo checkinstall -y -D --pkgversion="$DEBVER" --arch="arm64" --provides="box64" --conflicts="$conflict_list" --pkgname="box64-$target" --install="no" make install || error "Checkinstall failed to create a deb package."
+  sudo checkinstall -y -D --pkgversion="$DEBVER" --arch="arm64" --provides="box64" --conflicts="$conflict_list" --maintainer="Ryan Fortner <ryankfortner@gmail.com>" --pkglicense="MIT" --pkgsource="https://github.com/ptitSeb/box64" --pkggroup="utils" --pkgname="box64-$target" --install="no" make install || error "Checkinstall failed to create a deb package."
 
   cd $DIRECTORY
   mv box64/build/*.deb ./debian/ || error "Failed to move deb to debian folder."
